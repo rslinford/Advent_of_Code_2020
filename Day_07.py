1626c90e9ce4f0489d8ebb02026df63b14f4e66c
Scott <scott@linfords.com>
2022-08-01 21:16:50 -0700

    Day 07 completed, passes

--- Day_07.py
+++ Day_07.py
@@ -8,8 +8,9 @@ class Bag:
     instances = {}
 
     def __init__(self, fancy_color):
+        self.child_count = {}
         self.fancy_color = fancy_color
-        self.children = {}
+        self.children = []
 
     def __repr__(self):
         return f'Bag({self.fancy_color})'
@@ -22,16 +23,17 @@ class Bag:
 
     def render_children(self):
         rval = []
-        for i, child in enumerate(self.children.keys()):
+        for i, child in enumerate(self.children):
             if i > 0:
                 rval.append(', ')
-            rval.append(str(self.children.get(child)))
+            rval.append(str(self.child_count.get(child)))
             rval.append(' ')
             rval.append(str(child))
         return ''.join(rval)
 
-    def add_child(self, child_bag, can_contain_count):
-        self.children[child_bag] = can_contain_count
+    def add_child(self, child_bag, contains_count):
+        self.children.append(child_bag)
+        self.child_count[child_bag] = contains_count
 
 
 def bag_factory_fancy(fancy_color_name: str) -> Bag:
@@ -48,7 +50,7 @@ def bag_factory(adjective, color):
     return bag_factory_fancy(fancy_color_name)
 
 
-def bag_can_contain_shiny_gold(fancy_color):
+def bag_contains_shiny_gold(fancy_color):
     bag = bag_factory_fancy(fancy_color)
     if len(bag.children) == 0:
         return False
@@ -57,7 +59,7 @@ def bag_can_contain_shiny_gold(fancy_color):
             return True
     rval = False
     for child in bag.children:
-        rval = bag_can_contain_shiny_gold(child.fancy_color)
+        rval = bag_contains_shiny_gold(child.fancy_color)
         if rval is True:
             break
 
@@ -78,14 +80,14 @@ def load_bag_hierarchy(filename):
                 parent_bag.add_child(bag_factory(rlist[index], rlist[index + 1]), int(rlist[index - 1]))
                 # todo add as child
                 index += 4
-        # print(f'Created {parent_bag} with children: {parent_bag.render_children()}')
+        print(f'Created {parent_bag} with children: {parent_bag.render_children()}')
 
 
 def part1(filename):
     load_bag_hierarchy(filename)
     tally = 0
     for fancy_color in Bag.instances.keys():
-        if bag_can_contain_shiny_gold(fancy_color):
+        if bag_contains_shiny_gold(fancy_color):
             tally += 1
         # print(f'Bag {fancy_color}  can contain "shiny gold": {bag_can_contain_shiny_gold(fancy_color)}')
     print(f'Number of bags that can eventually contain "shiny gold": {tally}')
@@ -95,7 +97,27 @@ def part1(filename):
         assert (tally == 172)
 
 
+def tally_bags(bag):
+    tally = 0
+    for child in bag.children:
+        bags_held_by_child = tally_bags(child)
+        if bags_held_by_child != 0:
+            tally += bags_held_by_child * bag.child_count[child]
+    for child in bag.child_count.keys():
+        tally += bag.child_count[child]
+    return tally
+
+
+def part2(filename):
+    load_bag_hierarchy(filename)
+    gold_bag = bag_factory('shiny', 'gold')
+    total_bag_count = tally_bags(gold_bag)
+    print(f'The shiny gold bag contains {total_bag_count}')
+    if filename == 'Day_07_small_data.txt':
+        assert (total_bag_count == 32)
+    elif filename == 'Day_07_smaller_data':
+        assert (total_bag_count == 126)
 
 
 # part1('Day_07_small_data.txt')
-part1('Day_07_small_data.txt')
+part2('Day_07_data.txt')
