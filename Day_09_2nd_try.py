97bbe670deef1470d22de8521d467142ec0b781c
Scott <scott@linfords.com>
2022-08-02 22:21:37 -0700

    Day 9 - Conway in the Forth Dimension, passes

--- Day_09_2nd_try.py
+++ Day_09_2nd_try.py
@@ -15,7 +15,7 @@ def read_dimensions(filename):
 
 def read_initial_state(filename):
     width, height = read_dimensions(filename)
-    grid = np.zeros(((1, height, width)), dtype=int)
+    grid = np.zeros(((1, 1, height, width)), dtype=int)
     with open(filename, 'r') as f:
         for y, line in enumerate(f):
             line = line.strip()
@@ -23,7 +23,7 @@ def read_initial_state(filename):
                 if c == '.':
                     pass
                 elif c == '#':
-                    grid[0][y][x] = 1
+                    grid[0][0][y][x] = 1
                 else:
                     raise(ValueError, f'Found {c}  Expected a . or #')
 
@@ -31,44 +31,69 @@ def read_initial_state(filename):
         return grid
 
 offsets = [
-    (-1, -1, -1), (-1, -1, 0), (-1, -1, 1),
-    (-1, 0, -1),  (-1, 0, 0),  (-1, 0, 1),
-    (-1, 1, -1),  (-1, 1, 0),  (-1, 1, 1),
+    (-1, -1, -1, -1), (-1, -1, -1, 0), (-1, -1, -1, 1),
+    (-1, -1, 0, -1),  (-1, -1, 0, 0),  (-1, -1, 0, 1),
+    (-1, -1, 1, -1),  (-1, -1, 1, 0),  (-1, -1, 1, 1),
 
-    (0, -1, -1),  (0, -1, 0), (0, -1, 1),
-    (0, 0, -1),               (0, 0, 1),
-    (0, 1, -1),   (0, 1, 0),  (0, 1, 1),
+    (-1, 0, -1, -1),  (-1, 0, -1, 0), (-1, 0, -1, 1),
+    (-1, 0, 0, -1),   (-1, 0, 0, 0 ), (-1, 0, 0, 1),
+    (-1, 0, 1, -1),   (-1, 0, 1, 0),  (-1, 0, 1, 1),
 
-    (1, -1, -1),  (1, -1, 0), (1, -1, 1),
-    (1, 0, -1),   (1, 0, 0),  (1, 0, 1),
-    (1, 1, -1),   (1, 1, 0),  (1, 1, 1),
+    (-1, 1, -1, -1),  (-1, 1, -1, 0), (-1, 1, -1, 1),
+    (-1, 1, 0, -1),   (-1, 1, 0, 0),  (-1, 1, 0, 1),
+    (-1, 1, 1, -1),   (-1, 1, 1, 0),  (-1, 1, 1, 1),
+
+    (0, -1, -1, -1), (0, -1, -1, 0), (0, -1, -1, 1),
+    (0, -1, 0, -1),  (0, -1, 0, 0),  (0, -1, 0, 1),
+    (0, -1, 1, -1),  (0, -1, 1, 0),  (0, -1, 1, 1),
+
+    (0, 0, -1, -1), (0, 0, -1, 0), (0, 0, -1, 1),
+    (0, 0, 0, -1),                 (0, 0, 0, 1),
+    (0, 0, 1, -1),  (0, 0, 1, 0),  (0, 0, 1, 1),
+
+    (0, 1, -1, -1), (0, 1, -1, 0), (0, 1, -1, 1),
+    (0, 1, 0, -1),  (0, 1, 0, 0),  (0, 1, 0, 1),
+    (0, 1, 1, -1),  (0, 1, 1, 0),  (0, 1, 1, 1),
+
+    (1, -1, -1, -1), (1, -1, -1, 0), (1, -1, -1, 1),
+    (1, -1, 0, -1),  (1, -1, 0, 0),  (1, -1, 0, 1),
+    (1, -1, 1, -1),  (1, -1, 1, 0),  (1, -1, 1, 1),
+
+    (1, 0, -1, -1), (1, 0, -1, 0), (1, 0, -1, 1),
+    (1, 0, 0, -1),  (1, 0, 0, 0),  (1, 0, 0, 1),
+    (1, 0, 1, -1),  (1, 0, 1, 0),  (1, 0, 1, 1),
+
+    (1, 1, -1, -1), (1, 1, -1, 0),  (1, 1, -1, 1),
+    (1, 1, 0, -1),  (1, 1, 0, 0),   (1, 1, 0, 1),
+    (1, 1, 1, -1),  (1, 1, 1, 0),   (1, 1, 1, 1)
 ]
 
-def tally_active_neighbors(grid, z, y, x):
+def tally_active_neighbors(grid, w, z, y, x):
     tally = 0
     for offset in offsets:
-        if grid[z + offset[0]][y + offset[1]][x + offset[2]] == 1:
+        if grid[w + offset[0]][z + offset[1]][y + offset[2]][x + offset[3]] == 1:
             tally += 1
     return tally
 
 def perform_cycle(grid):
     grid = grow_grid(grid)
     r_grid = np.zeros(grid.shape, dtype=int)
-    for z in range(1, grid.shape[0] - 1):
-        for y in range(1, grid.shape[1] - 1):
-            for x in range(1, grid.shape[2] - 1):
-                tally = tally_active_neighbors(grid, z, y, x)
-                is_active = grid[z][y][x] == 1
-                if is_active:
-                    if tally == 2 or tally == 3:
-                        r_grid[z][y][x] = 1
+    for w in range(1, grid.shape[0] - 1):
+        for z in range(1, grid.shape[1] - 1):
+            for y in range(1, grid.shape[2] - 1):
+                for x in range(1, grid.shape[3] - 1):
+                    tally = tally_active_neighbors(grid, w, z, y, x)
+                    is_active = grid[w][z][y][x] == 1
+                    if is_active:
+                        if tally == 2 or tally == 3:
+                            r_grid[w][z][y][x] = 1
+                        else:
+                            r_grid[w][z][y][x] = 0
                     else:
-                        r_grid[z][y][x] = 0
-                else:
-                    if tally == 3:
-                        r_grid[z][y][x] = 1
-                    else:
-                        r_grid[z][y][x] = 0
+                        if tally == 3:
+                            r_grid[w][z][y][x] = 1
+                        else:
+                            r_grid[w][z][y][x] = 0
 
     return r_grid
 
@@ -80,19 +105,22 @@ def perform_six_cycles(grid):
     return grid
 
 def grow_grid(grid):
-    return np.pad(grid, ((1, 1), (1, 1), (1, 1)), 'constant')
+    return np.pad(grid, ((1, 1), (1, 1), (1, 1), (1, 1)), 'constant')
 
 
 def render_grid(grid):
     rval = []
-    for z in range(0, grid.shape[0]):
-        rval.append(f'z = {z}\n')
-        for y in range(0, grid.shape[1]):
-            for x in range(0, grid.shape[2]):
-                if grid[z][y][x] == 1:
-                    rval.append('#')
-                else:
-                    rval.append('.')
+    for w in range(0, grid.shape[0]):
+        rval.append(f'****  w = {w}  ****\n')
+        for z in range(0, grid.shape[1]):
+            rval.append(f'z = {z}\n')
+            for y in range(0, grid.shape[2]):
+                for x in range(0, grid.shape[3]):
+                    if grid[w][z][y][x] == 1:
+                        rval.append('#')
+                    else:
+                        rval.append('.')
+                rval.append('\n')
             rval.append('\n')
         rval.append('\n')
     return ''.join(rval)
@@ -133,27 +161,27 @@ class TestConway(unittest.TestCase):
 
     def test_read_initial_state(self):
         grid = read_initial_state('Day_09_short_data_2.txt')
-        self.assertEqual((3, 5, 5), grid.shape)
-        self.assertEqual(1, grid[1][3][3])
+        self.assertEqual((3, 3, 5, 5), grid.shape)
+        self.assertEqual(1, grid[1][1][3][3])
 
     def test_grow_grid(self):
         grid = read_initial_state('Day_09_short_data_2.txt')
-        self.assertEqual((3, 5, 5), grid.shape)
-        self.assertEqual(0, grid[1][0][0])
-        self.assertEqual(1, grid[1][1][1])
+        self.assertEqual((3, 3, 5, 5), grid.shape)
+        self.assertEqual(0, grid[1][1][0][0])
+        self.assertEqual(1, grid[1][1][1][1])
         grid = grow_grid(grid)
-        self.assertEqual((5, 7, 7), grid.shape)
-        self.assertEqual(0, grid[2][1][1])
-        self.assertEqual(1, grid[2][2][2])
-        self.assertEqual(1, grid[2][4][2])
+        self.assertEqual((5, 5, 7, 7), grid.shape)
+        self.assertEqual(0, grid[1][2][1][1])
+        self.assertEqual(0, grid[1][2][2][2])
+        self.assertEqual(0, grid[1][2][4][2])
 
     def test_perform_cycle(self):
         grid = read_initial_state('Day_09_short_data.txt')
         grid = perform_cycle(grid)
-        self.assertEqual(0, grid[1][1][1])
-        self.assertEqual(1, grid[1][3][2])
-        self.assertEqual(0, grid[0][1][1])
-        self.assertEqual(0, grid[0][2][1])
+        self.assertEqual(0, grid[1][1][1][1])
+        self.assertEqual(1, grid[1][1][3][2])
+        self.assertEqual(0, grid[1][0][1][1])
+        self.assertEqual(1, grid[1][1][3][2])
         self.assertTrue(grid.any())
 
     def test_perform_six_cycle(self):
@@ -163,9 +191,9 @@ class TestConway(unittest.TestCase):
 
     def test_tally_active_neighbors(self):
         grid = read_initial_state('Day_09_short_data_2.txt')
-        tally = tally_active_neighbors(grid, 1, 0, 0)
+        tally = tally_active_neighbors(grid, 1, 1, 0, 0)
         self.assertEqual(1, tally)
-        tally = tally_active_neighbors(grid, 1, 2, 2)
+        tally = tally_active_neighbors(grid, 1, 1, 2, 2)
         self.assertEqual(8, tally)
-        tally = tally_active_neighbors(grid, 1, 3, 1)
+        tally = tally_active_neighbors(grid, 1, 1, 3, 1)
         self.assertEqual(2, tally)
